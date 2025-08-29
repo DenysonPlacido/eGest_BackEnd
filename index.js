85f8e4994c0a795720da845c4dec6c60569dceae
Denyson <132364948+DenysonPlacido@users.noreply.github.com>
2025-08-29 19:23:32 +0000

    6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/auth.js';
-import empresasRoutes from './routes/empresas.js';
+import empresasRoutes from './routes/auth.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
