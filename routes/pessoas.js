1793b7c4c9d43eaec2eeccdace4dea6d6537f967
Denyson <132364948+DenysonPlacido@users.noreply.github.com>
2025-09-14 16:16:35 +0000

    35

--- routes/pessoas.js
+++ routes/pessoas.js
@@ -1,3 +1,5 @@
+// /workspaces/eGest_BackEnd/routes/pessoas.js
+
 import express from 'express';
 import { pool } from '../db.js';
 
@@ -16,15 +18,17 @@ router.post('/pessoas/gerenciar', async (req, res) => {
   try {
     if (acao === 'SELECT') {
       await client.query('BEGIN');
-      await client.query(`
-        CALL gerenciar_pessoa(
-          $1, $2, $3, $4, $5, $6, $7, $8, $9,
-          $10, $11, $12, $13, $14, $15
-        )
-      `, [
-        acao, pessoa_id, tipo_pessoa, cpf_cnpj, nome, data_nascimento,
-        ddd, fone, email, cep, cod_logradouro, numero, cod_bairro, complemento, 'resultado_cursor'
-      ]);
+ await client.query(`
+  CALL gerenciar_pessoa(
+    $1, $2, $3, $4, $5, $6, $7, $8, $9,
+    $10, $11, $12, $13, $14, $15,
+    $16, $17
+  )
+`, [
+  acao, pessoa_id, tipo_pessoa, cpf_cnpj, nome, data_nascimento,
+  ddd, fone, email, cep, cod_logradouro, numero, cod_bairro, complemento,
+  'resultado_cursor', limit, offset
+]);
       const result = await client.query('FETCH ALL IN resultado_cursor');
       await client.query('COMMIT');
       res.json(result.rows);
